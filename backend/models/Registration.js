a586a03f9cf94c17355abb1e74b4a39e738bcbb0
Md <devsync84@gmail.com>
2025-06-15 22:13:03 +0300

    Update Registration.js

--- backend/models/Registration.js
+++ backend/models/Registration.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const registrationSchema = new mongoose.Schema({
+    sectionName: { type: String, required: true },
+    householdName: { type: String, required: true },
+    gender: { type: String, required: true },
+    city: { type: String, default: "Kebridahar" },
+    kebelle: { type: String, required: true },
+    idNumber: { type: String, required: true },
+    phone: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    createdBy: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Registration', registrationSchema);
